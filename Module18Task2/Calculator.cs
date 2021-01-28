0a04db9966661bf791328cf653d97e682690e7ac
rePakkun <cj.ilya@yandex.ru>
2021-01-28 14:51:18 +0300

    Добавьте файлы проекта.

---
+++ Module18Task2/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Module18Task2.Program;
+
+namespace Module18Task2
+{
+    class Calculator : ICalculator
+    {
+        ILogger Logger { get; }
+        public Calculator(ILogger logger)
+        {
+            Logger = logger;
+        }     
+        public void Addition(int a, int b)
+        {
+            Logger.Event($"Сумма = {a + b}");
+        }
+    }
+}
---
+++ Module18Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Module18Task2
+{
+    class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main(string[] args)
+        {
+            Logger = new Logger1();
+            var calc = new Calculator(Logger);
+            try
+            {
+                calc.Addition(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+            }
+            catch(FormatException message)
+            {
+                Logger.Error(message.Message);
+            }
+
+            string cringe = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Red;           
+            Console.ReadKey();
+        }
+              
+        public interface ILogger
+        {
+            void Event(string message);
+            void Error(string message);
+        }
+        public class Logger1 : ILogger
+        {
+            public void Error(string message)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+            }
+
+            public void Event(string message)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(message);
+            }
+        }
+        public interface ICalculator
+        {
+            void Addition(int a, int b);
+        }
+    }
+    
+}
